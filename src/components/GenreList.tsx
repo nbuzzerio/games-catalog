e8981c445931b5bb8978b592c6b9a7cf7627e4b3
nbuzzerio <nick.buzzerio@hotmail.com>
2024-02-07 20:35:17 -0500

    styled genres panel and added images from api

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,24 @@
 import useGenres from "../hooks/useGenres";
+import { getCroppedImageUrl } from "../services/img-url";
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <ul className="list-unstyled">
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <li
+          key={genre.id}
+          className="my-3 d-flex gap-3 align-items-center cursor-pointer"
+        >
+          <img
+            src={getCroppedImageUrl(genre.image_background)}
+            alt=""
+            className="img-fluid rounded-3 "
+            style={{ width: "3rem", height: "3rem" }}
+          />
+          <span className="fs-5">{genre.name}</span>
+        </li>
       ))}
     </ul>
   );
