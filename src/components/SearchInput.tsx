ffa5983af8fbf9a7d412e4406fc366ef810448b9
Nick Buzzerio <nick.buzzerio@hotmail.com>
2024-02-24 18:17:35 -0500

    updated SearchInput and added it to navbar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,6 +1,15 @@
+import { BsSearch } from "react-icons/bs";
+
 const SearchInput = () => {
   return (
-    <input type="text" placeholder="Search games..." className="rounded-3 " />
+    <div className="w-100 d-flex position-relative">
+      <BsSearch className="input-icon" />
+      <input
+        type="text"
+        placeholder="Search games..."
+        className="rounded-2 text-bg-dark px-5 py-1 w-100"
+      />
+    </div>
   );
 };
 
