0b3d628395401a71225a7b9af5161741874bf50b
Nick Buzzerio <nick.buzzerio@hotmail.com>
2024-02-27 18:39:12 -0500

    refactored find code for platforms and genres into hooks

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,19 +1,14 @@
 import { GameQuery } from "../App";
-import useGenres from "../hooks/useGenres";
-import usePlatforms from "../hooks/usePlatforms";
+import useGenre from "../hooks/useGenre";
+import usePlatform from "../hooks/usePlatform";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: genres } = useGenres();
-  const { data: platforms } = usePlatforms();
-
-  const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
-  const platform = platforms?.results.find(
-    (g) => g.id === gameQuery.platformId,
-  );
+  const genre = useGenre(gameQuery.genreId);
+  const platform = usePlatform(gameQuery.platformId);
 
   return (
     <h1 className="fs-1 px-4 py-4">
